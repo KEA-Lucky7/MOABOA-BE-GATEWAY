b2c92d0c5045e433007b8a0348c1be761dbebfdf
hyunseok <hyun104467@naver.com>
2024-04-11 13:42:23 +0900

    KAN-78-[CHORE]: gitignore 처리 및 프로젝트 생성

---
+++ src/main/java/moaboa/gateway/MoaboaGatewayApplication.java
@@ -0,0 +1,13 @@
+package moaboa.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaboaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoaboaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moaboa/gateway/MoaboaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package moaboa.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaboaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
